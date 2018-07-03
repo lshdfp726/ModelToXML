dc9fb81af813ecea6e8464c4d5d8ef3e3f28d003
刘松洪 <liusonghong@liusonghongs-mac-mini.local>
2018-07-03 16:19:10 +0800

    Initial Commit

---
+++ DataAnalysis/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DataAnalysis
+//
+//  Created by 刘松洪 on 2018/7/3.
+//  Copyright © 2018年 刘松洪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DataAnalysisTests/DataAnalysisTests.m
@@ -0,0 +1,39 @@
+//
+//  DataAnalysisTests.m
+//  DataAnalysisTests
+//
+//  Created by 刘松洪 on 2018/7/3.
+//  Copyright © 2018年 刘松洪. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DataAnalysisTests : XCTestCase
+
+@end
+
+@implementation DataAnalysisTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
